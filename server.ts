7f885994c00ca1dc7d8d242a744f45db9715b067
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2025-04-02 21:21:08 -0300

    ** Exchange default react-router web server for Elysia web server.

---
+++ server.ts
@@ -0,0 +1,25 @@
+// https://github.com/kravetsone/elysia-react-router/blob/main/example/server/index.ts
+
+import { Elysia } from "elysia"
+import { reactRouter } from "elysia-react-router"
+
+const port = Number(process.env.PORT) || 3000
+
+new Elysia()
+  .use(
+    await reactRouter({
+      getLoadContext: () => ({ hotPostName: "some post title" }),
+    })
+  )
+
+  .listen(port, () => {
+    console.log(
+      `Elysia server is running on ${process.env.NODE_ENV} mode at http://localhost:${port}`
+    )
+  })
+
+declare module "react-router" {
+  interface AppLoadContext {
+    hotPostName: string
+  }
+}
