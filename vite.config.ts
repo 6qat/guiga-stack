df339003f5d94e1b99da4dadb7ec94e568caa3ee
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2025-04-02 21:12:32 -0300

    Add biome for lint and format code. Format all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	css: {
-		postcss: {
-			plugins: [],
-		},
-	},
-	plugins: [reactRouter(), tsconfigPaths()],
-	resolve:
-		process.env.NODE_ENV === "development"
-			? {}
-			: {
-					alias: {
-						"react-dom/server": "react-dom/server.node",
-					},
-				},
-});
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+  resolve:
+    process.env.NODE_ENV === "development"
+      ? {}
+      : {
+          alias: {
+            "react-dom/server": "react-dom/server.node",
+          },
+        },
+})
